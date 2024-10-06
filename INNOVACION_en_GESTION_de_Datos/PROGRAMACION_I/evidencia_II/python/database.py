7d2f0250271937a3688f2e10acf648d429138602
PaoTes <andreaptesta@gmail.com>
2024-10-06 20:26:17 -0300

    corrección archivo database.py

--- INNOVACION_en_GESTION_de_Datos/PROGRAMACION_I/evidencia_II/python/database.py
+++ INNOVACION_en_GESTION_de_Datos/PROGRAMACION_I/evidencia_II/python/database.py
@@ -1,9 +1,10 @@
 import mysql.connector
 def get_db_connection():
     connection = mysql.connector.connect(
-        host='localhost',    
+        host='localhost',
         user='root',         
         password='tu clave', 
-        database='tu base de datos' 
+        database='gestion_usuarios',
+        port=3306
     )
     return connection
