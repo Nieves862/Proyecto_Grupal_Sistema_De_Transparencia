ca56e1f837b220195b5a9ffc6717f35ae4db2cdc
v.nieves.v <queiobailaphotography@gmail.com>
2025-04-27 17:20:30 -0300

    subiendo actividades en .js

---
+++ segundo_2025/primer_cuatrimestre/Proyecto_Integrador/Ev_I/Diagnostico/actividad_1_1.js
@@ -0,0 +1,6 @@
+// Declaración de las variables
+let nombre = "Valeria Nieves";
+let edad = 45;
+
+// Imprimir el mensaje en la consola
+console.log("Hola, me llamo " + nombre + " y tengo " + edad + " años.");
