8286120b234f14be68330b0dfb231b40cf3711f9
v.nieves.v <queiobailaphotography@gmail.com>
2025-04-28 22:27:17 -0300

    eliminando

--- segundo_2025/primer_cuatrimestre/Proyecto_Integrador/Ev_I/Diagnostico/actividad_1_1.js
+++
@@ -1,6 +0,0 @@
-// Declaración de las variables
-let nombre = "Valeria Nieves";
-let edad = 45;
-
-// Imprimir el mensaje en la consola
-console.log("Hola, me llamo " + nombre + " y tengo " + edad + " años.");
